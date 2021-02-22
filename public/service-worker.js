ae90db239fe7ba5bb829f1f56c4b0ac08a9044b9
RogersJohn811 <rogersjohn811@gmail.com>
2021-02-21 20:58:59 -0500

    Working on service worker

---
+++ public/service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    "/manifest.webmanifest",
+    "/index.js",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+];
+
+self.addEventListener("install", function (evt) {
+    // pre CACHE TRANSACTION DATA
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+
+   // tell the browser to activate this service worker immediately once it
+  // has finished installing
+  self.skipWaiting();
+});

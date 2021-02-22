31aa30f4df3e5af32e4383e4d401f7f8e4be3578
RogersJohn811 <rogersjohn811@gmail.com>
2021-02-21 21:12:10 -0500

    Working on service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -11,7 +11,6 @@ const FILES_TO_CACHE = [
 ];
 
 self.addEventListener("install", function (evt) {
-    // pre CACHE TRANSACTION DATA
     evt.waitUntil(
       caches.open(CACHE_NAME).then(cache => {
         console.log("Your files were pre-cached successfully!");
@@ -19,7 +18,52 @@ self.addEventListener("install", function (evt) {
       })
     );
 
-   // tell the browser to activate this service worker immediately once it
-  // has finished installing
   self.skipWaiting();
 });
+
+self.addEventListener("activate", function(evt) {
+    evt.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
+  });
+
+  self.addEventListener("fetch", function(evt) {
+    if (evt.request.url.includes("/api/")) {
+      evt.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(evt.request)
+            .then(response => {
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+  
+              return response;
+            })
+            .catch(err => {
+              return cache.match(evt.request);
+            });
+        }).catch(err => console.log(err))
+      );
+  
+      return;
+    }
+  
+    evt.respondWith(
+      caches.open(CACHE_NAME).then(cache => {
+        return cache.match(evt.request).then(response => {
+          return response || fetch(evt.request);
+        });
+      })
+    );
+  });
